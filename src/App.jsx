fa047c1a177fde8db5a0921381c125371899e447
AlejandroSZIOSA <ggato33@gmail.com>
2024-01-09 12:39:21 +0100

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,71 @@
-import { useState, Text } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  function Button() {
+  function CustomBtn(props) {
     return (
       <div>
-        <Text> Hola</Text>
+        <button
+          style={{
+            backgroundColor: props.color,
+            padding: props.size,
+            disabled: false,
+          }}
+        >
+          Buttons
+        </button>
       </div>
     );
   }
 
-  const [count, setCount] = useState(0);
+  function CustomAlert(props) {
+    return (
+      <div
+        style={{
+          display: "Flex",
+          flexDirection: "row",
+          backgroundColor: props.color,
+          alignItems: "center",
+          border: "1px solid gray",
+          borderRadius: "10px",
+        }}
+      >
+        <div style={{ padding: "10px" }}>
+          <img width="40px" height="40px" src={props.icon}></img>
+        </div>
+        <div
+          style={{
+            fontSize: "xx-large",
+            paddingLeft: "5px",
+            paddingRight: "5px",
+          }}
+        >
+          Alert Message
+        </div>
+      </div>
+    );
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Custom Components</h1>
+      <div className="content-container">
+        <h2>Buttons</h2>
+        <div className="btn-container">
+          <CustomBtn color="red" size="20px 15px 20px 15px" />
+          <CustomBtn color="gray" size="10px 15px 10px 15px" />
+          <CustomBtn color="orange" size="5px 25px 5px 25px" />
+        </div>
+
+        <h2>Alerts</h2>
+        <div className="alerts-container">
+          <CustomAlert color="red" icon="/src/assets/alert-icon1.png" />
+          <CustomAlert color="blue" icon="/src/assets/alert-icon2.png" />
+          <CustomAlert color="green" icon="/src/assets/alert-icon3.png" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
