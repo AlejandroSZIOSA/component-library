a994f486f71932b3b4673958db1b894d66a719fa
AlejandroSZIOSA <ggato33@gmail.com>
2024-01-09 09:32:04 +0100

    Initial Commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, Text } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function Button() {
+    return (
+      <div>
+        <Text> Hola</Text>
+      </div>
+    );
+  }
+
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
